65450ce29a765120fc3f0c35c1569d4aae415adb
kohlbrr <kohlbrr@gmail.com>
2017-05-19 12:28:54 -0400

    Got dat middleware

--- browser/react/store.js
+++ browser/react/store.js
@@ -2,5 +2,7 @@
 
 import { createStore } from 'redux';
 import reducer from './reducers/root-reducer';
+import { applyMiddleware } from 'redux';
+import loggerMiddleware from 'redux-logger';
 
-export default createStore(reducer);
+export default createStore(reducer, applyMiddleware(loggerMiddleware));
